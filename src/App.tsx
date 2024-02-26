5bce34dcb3534a6f2fc2506308cf238cdbd7a598
uniyalmani <ashutoshuniyal21@gmail.com>
2024-02-27 00:08:50 +0530

    adding share feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import { Outlet } from 'react-router-dom';
 
+
+
 function App() {
   return (
     <>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,13 @@
 // src/utils/constants.ts
+//54.162.94.234
 export const API_BASE_URL =  'http://54.162.94.234/api/'; 
 export const SIGNIN_ENDPOINT = 'auth/login/';
+export const REFRESH_TOKEN_ENDPOINT = 'auth/refresh-token/'
 export const SIGNUP_ENDPOINT = 'auth/signup/';
 export const NOTES_ENDPOINT = 'notes/';
 export const CREATE_NOTE_ENDPOINT = 'notes/createnote/';
 export const TOGGLE_PIN_ENDPOINT = 'notes/togglepin/';
+export const GENERATE_AND_ACCESS_SHARED_NOTE_ENDPOINT = 'notes/shared/';
+export const GET_NOTE_BY_PUBLIC_ID_ENDPOINT = 'notes/public/';
 
 export const MAX_SAVE_ATTEMPTS = 3;
